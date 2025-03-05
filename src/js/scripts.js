a0a741421f2a672a60e08e68037985a6d37c1cb2
Zavorotna <zavorotnaa96@gmail.com>
2025-03-05 03:17:37 +0200

    Create repo project mdm, start work, create structure, header, first section, fade out, run stroke, add style, burder menu, adaptive

---
+++ src/js/scripts.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function() {
+    /*move text page*/
+    const carousel = document.querySelector('.content-slider')
+    if (document.querySelector(".content-slider h3")) {
+        let items = [...document.querySelectorAll(".content-slider h3")],
+            itemImgWidth = items[0].offsetWidth,
+            isAnimatingImg = false,
+            gap = carousel.offsetWidth
+
+        function updateCarouselImg() {
+            while (carousel.firstChild) {
+                carousel.removeChild(carousel.firstChild)
+            }
+            for (let i = 0; i < items.length; i++) {
+                const cloneImg = items[i].cloneNode(true)
+                carousel.appendChild(cloneImg)
+            }
+
+        }
+        updateCarouselImg()
+
+        function startAutoScroll() {
+            let currentOffset = 0
+
+            setInterval(() => {
+                if (!isAnimatingImg) {
+                    isAnimatingImg = true
+                    currentOffset -= 1;
+
+                    carousel.style.transition = 'none'
+                    carousel.style.transform = `translateX(${currentOffset}px)`
+
+                    if (Math.abs(currentOffset) >= itemImgWidth + 50) {
+                        currentOffset = 0;
+                        updateCarouselImg()
+                    }
+                    isAnimatingImg = false
+                }
+            }, 20)
+        }
+        startAutoScroll()
+    }
+    //fade out for main container
+    if (document.querySelector('.slider-images-1')) {
+        const images1 = document.querySelectorAll('.slider-images-1 img'),
+            images2 = document.querySelectorAll('.slider-images-2 img')
+        let currentImgIndex = 0;
+    
+        images1[0].classList.add('active')
+        images2[0].classList.add('active')
+    
+        setInterval(() => {
+            images1[currentImgIndex].classList.remove('active')
+            images2[currentImgIndex].classList.remove('active')
+            currentImgIndex = (currentImgIndex + 1) % images1.length
+            images1[currentImgIndex].classList.add('active')
+            images2[currentImgIndex].classList.add('active')
+        }, 3000)
+    }
+
+    //burger
+    if(document.querySelector(".burger")) {
+        const dark = document.querySelector(".dark-bgc"),
+          burger = document.querySelector(".burger"),
+          menu = document.querySelector(".nav"),
+          cancel = document.querySelector(".cancel")
+    
+        burger.addEventListener("click", function() {
+            menu.style.left = "0";
+            dark.style.display = "block"
+        })
+        function cancelBurger() {
+            menu.style.left = "-100%";
+            dark.style.display = "none"
+        }
+        cancel.addEventListener("click", cancelBurger)
+        dark.addEventListener("click", cancelBurger)
+    }
+
+
+    
+
+})
